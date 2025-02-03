bdc12addaf081271e1ac57d60ff8a23f98c5aa3e
thisismeaji <thisismeaji20@gmail.com>
2025-02-03 09:14:30 +0700

    add navigation footer and font

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Styles from "../footer/footer.module.css";
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Footer() {
+  return (
+    <footer className={Styles.footer}>
+      <div>
+        <div>
+            <p>© 2025 Ajisaka Kamandanu. All Rights Reserved.</p>
+        </div>
+        <div>
+            <p>Connect with me</p>
+            <ul>
+                <li>
+                    <Link href="https://www.instagram.com/thisisme_aji/">
+                        <Image
+                            src="/assets/icons/instagram.png"
+                            alt="Instagram Aji"
+                            width={1000}
+                            height={1000}
+                        />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="https://www.facebook.com/profile.php?id=100090748616081">
+                        <Image
+                            src="/assets/icons/facebook.png"
+                            alt="facebook Aji"
+                            width={1000}
+                            height={1000}
+                        />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="https://www.threads.net/@thisisme_aji">
+                        <Image
+                            src="/assets/icons/threads.png"
+                            alt="threads Aji"
+                            width={1000}
+                            height={1000}
+                        />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="https://x.com/thisismeaji">
+                        <Image
+                            src="/assets/icons/twitter.png"
+                            alt="twitter Aji"
+                            width={1000}
+                            height={1000}
+                        />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/linkedin">
+                        <Image
+                            src="/assets/icons/linkedin.png"
+                            alt="linkedin Aji"
+                            width={1000}
+                            height={1000}
+                        />
+                    </Link>
+                </li>
+            </ul>
+        </div>
+      </div>
+    </footer>
+  )
+}
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,85 @@
+"use client";
+
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Styles from "../navigation/navigation.module.css";
+
+export default function Navigation() {
+    const [isNavActive, setIsNavActive] = useState(false);
+
+    // Fungsi untuk toggle menu
+    const toggleNav = () => {
+        setIsNavActive((prev) => !prev);
+    };
+
+    // Fungsi untuk close menu
+    const handleClose = () => {
+        setIsNavActive(false);
+    };
+
+    // Fungsi untuk close menu sebelum navigasi
+    const handleNavLinkClick = (href) => {
+        setIsNavActive(false);
+    };
+
+    return (
+        <nav className={Styles.nav}>
+            <Link href="/">
+                <div className={Styles.logo}>
+                    <Image
+                        src="/assets/images/logo.png"
+                        alt="Logo"
+                        width="1000"
+                        height="1000"
+                    />
+                </div>
+            </Link>
+            <div className={Styles.navList}>
+                {/* Desktop Navigation */}
+                <ul className={Styles.navDekstop}>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link href="/portfolio">Portfolio</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">Contact</Link>
+                    </li>
+                </ul>
+
+                {/* Mobile Navigation */}
+                <ul className={`${Styles.navMobile} ${isNavActive ? Styles.navActive : ""}`}>
+                    <li>
+                        <Link href="/" onClick={handleNavLinkClick}>Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/about" onClick={handleNavLinkClick}>About</Link>
+                    </li>
+                    <li>
+                        <Link href="/portfolio" onClick={handleNavLinkClick}>Portfolio</Link>
+                    </li>
+                    <li>
+                        <Link href="/contact" onClick={handleNavLinkClick}>Contact</Link>
+                    </li>
+                    {/* Tombol Close */}
+                    <li className={Styles.closeButton} onClick={handleClose}>×</li>
+                </ul>
+
+                {/* Overlay */}
+                {isNavActive && <div className={Styles.overlay} onClick={handleClose}></div>}
+            </div>
+
+            {/* Menu Button */}
+            <div className={Styles.menu} onClick={toggleNav}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>
+    );
+}
