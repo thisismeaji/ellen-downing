5b445fcf2c5f9e362dca8753f569e21032fa8504
thisismeaji <thisismeaji20@gmail.com>
2025-02-03 13:25:17 +0700

    add more sample Work

--- src/components/section/work/WorkSection.js
+++ src/components/section/work/WorkSection.js
@@ -11,6 +11,28 @@ export default function WorkSection() {
                 <h1>Sample Work</h1>
             </div>
             <Work/>
+            <div className={Styles.blooming}>
+              <div>
+                <Image
+                  src="/assets/images/blooming-one.png"
+                  alt="Blooming"
+                  width={1000}
+                  height={1000}
+                />
+              </div>
+              <div>
+                <Image
+                  src="/assets/images/blooming-two.png"
+                  alt="Blooming"
+                  width={1000}
+                  height={1000}
+                />
+                <h2>Blooming</h2>
+                <p>Dimensions: 20 x 20</p>
+                <p>Concept: Secret Corners</p>
+                <p>Year: 2023</p>
+              </div>
+            </div>
         </div>
     </section>
   )
